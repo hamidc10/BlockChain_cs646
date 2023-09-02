3e40bc81123240c2c1e9a0fab5d8bdc337a0bba0
Hamid <hac@DESKTOP-B6ROD7G.localdomain>
2023-09-02 12:22:53 -0500

    Transaction python rework

--- transaction.py
+++ transaction.py
@@ -39,6 +39,7 @@ class Transaction_info:
     Data_str=str(Data)
     Data_str=Data_str.replace(" ","")
     file_name=hashlib.sha256(Data_str.encode('utf-8')).hexdigest()
+
     
     with open(file_name+".json","w") as f:
         json.dump(Data,f,indent=None)
