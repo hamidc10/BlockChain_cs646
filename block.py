31eea6cf2f8a6ce44498ee4f19b0cdc7a6eb29b8
Chantelrw <122568264+Chantelrw@users.noreply.github.com>
2023-09-08 05:51:59 +0000

    wip: block file basic logic

--- block.py
+++ block.py
@@ -21,16 +21,67 @@ class Block:
     body: dict[str:list[dict]]
 
 class processed_transaction:
-    def __init__(self,Block):
-        self.block = Block
+    
+    def __init__(self,Height,Timestamp,Previous, Hash):
+        self.height=Height
+        self.timestamp=Timestamp
+        self.previousblock=Previous
+        self.hash=Hash
 
-    path_to_pending="/workspaces/BlockChain_cs646/pending_transaction/"
-    blocks=list
+    # This gets the input from the transaction.py file
+    # Once the data is received it then get put into a dictionary which would be converted to JSON
+    # Used this for help with JSON writing to file:
+    #    -https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
 
-    for file_name in os.listdir(path_to_pending):
-        print(file_name.split(".")[0])
+    
+    # Before we save the file we need to hash it by using SHA256
+    # I took the dictionary and make it into a str with no space then I can hash it using hashlib library
+    # After we hash it, The file for the JSON will be saved as hash we created
+    # Used this to help me understand how hashlib worked
+    #   -https://docs.python.org/3/library/hashlib.html 
+    # I had an error with "String must be encoded" found this to help
+    #   -https://bobbyhadz.com/blog/python-typeerror-strings-must-be-encoded-before-hashing
 
 
+        
+    folder_name="/workspaces/BlockChain_cs646/pending_transaction/"
+    Height=0
+    current_time=datetime.datetime.now()
+    Timestamp=int(datetime.datetime.timestamp(current_time))
+    if(Height==0):
+        Previous="NA"
+    else: 
+        Previous=self.Previous
+        
+    body_list=[]
+
+    with open(folder_name+file_name+".json","r") as f:
+        body_dict={"hash":file_name,"content":f}
+        body_list.append(body_dict)
+        print(body_dict)
+    
+    body={"body":body_list}
+    Body_str=str(body)
+    Body_str=Body_str.replace(" ","")
+    Hash=hashlib.sha256(Body_str.encode('utf-8')).hexdigest()
+    
+    header_dict={
+        "height": Height,
+        "timestamp":Timestamp,
+        "previousblock":Previous,
+        "hash":Hash
+    }
+
+    header={"header":header_dict}
+    Header_str=str(header)
+    Header_str=Header_str.replace(" ","")
+    blocks="/workspaces/BlockChain_cs646/blocks/"
+    file_name=hashlib.sha256(Header_str.encode('utf-8')).hexdigest()
+
+    
+    with open(blocks+file_name+".json","w") as f:
+        json.dump(Header_str,f,indent=None)
+    
 
 #Used this to help me understand the different terminologies: https://www.geeksforgeeks.org/important-blockchain-terminologies/?ref=gcse
 '''class Block:
