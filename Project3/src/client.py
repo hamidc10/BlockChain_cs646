f9761e642aa6079c8a043cd380a4c7b1c28bae7d
xavier123456789900-0 <122751239+xavier123456789900-0@users.noreply.github.com>
2023-10-12 18:16:03 +0000

    Fix all errors

--- Project3/src/client.py
+++ Project3/src/client.py
@@ -1,16 +1,15 @@
 #code that establishes connection.
+#Important note you have to delete the terminal 
+#then new terminal when you stop the connection of the server to client.
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #server we want client to connect to
-
-# Should set this up so it can be implemented in each node i believe? 
-
-
-server = ("127.0.0.1",5555)
-s.connect(server)
+# server = ("127.0.0.1",5555)
+# s.connect(server)
+s.connect(("127.0.0.1",5555))
 response = s.recv(100)
-print(response.decode("UFT-8"))
+print(response.decode("UTF-8"))
 
 message = "Test"
 message = message.encode()
@@ -19,12 +18,11 @@ s.send(message)
 response = s.recv(100)
 my_size = int(response)
 
-
 content = s.recv(my_size)
+
 f = open("Newfile.txt", "wb")
 f.write(content)
 f.close()
-#print(response.decode("UTF-8"))
 
-#closes connection
+
 s.close()
--- Project3/src/server.py
+++ Project3/src/server.py
@@ -9,30 +9,22 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # what ip/port we want the server to be on
 server = ("127.0.0.1",5555)
 s.bind(server)
-
 #establishes how many connections we want the server to listen for
 s.listen(5)
 
-# loop to keep connection going and send messages, should expand on this potentially to have communications between nodes?
+# loop to keep connection going
 while True:
     clientsocket, address = s.accept()
     print ("connection from " + str(address) + " has been established!")
 
     clientsocket.send(b"Connection Established")
 
-    my_message = clientsocket.recv(100)
-    print(my_message.decode("UTF-8)"))
-
-    my_message = clientsocket.recv(100)
+    my_message =clientsocket.recv(100)
     print(my_message.decode("UTF-8"))
-    
+
     clientsocket.send(str(file_size).encode())
 
-
-    #clientsocket.send(b"Goodbye")
-    f = open("myfile.txt", "rb")
+    f = open("myfile.txt","rb")
     content = f.read()
     f.close()
     clientsocket.send(content)
-
-# need to set up error handling so that way we do not keep getting "address in use" errors.
